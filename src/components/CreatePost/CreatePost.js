330a1f85c4f4c0eef1c1172777d20c79c655b31e
Kartik4152 <gaming4152@gmail.com>
2021-04-05 22:37:39 +0530

    added tab

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import './CreatePost.css';
 
 const CreatePost=(props)=>{
     const maxLength=420;
     const [rant,setRant]=useState('');
+    const textAreaRef=useRef(null);
     const sendRant = async ()=>{
         if(rant.length&&rant.length<=maxLength)
         {
@@ -20,14 +21,30 @@ const CreatePost=(props)=>{
             setRant('');
             props.setRerender(Math.random())
         }
-else
+        else
             alert('Some Error occured ;-;');
         }
     }
+    useEffect(()=>{
+        const addTab=(e)=>{
+        if(e.key==='Tab'&&rant.length<420)
+            {
+                e.preventDefault();
+                let start=textAreaRef.current.selectionStart;
+                let end=textAreaRef.current.selectionEnd;
+                setRant(rant.substring(0,start)+'\t'+rant.substring(end));
+                textAreaRef.current.selectionStart=textAreaRef.current.selectionEnd=start+1;
+            }
+        }
+        textAreaRef.current.addEventListener('keydown',addTab);
+        return ()=>{
+            textAreaRef.current.removeEventListener('keydown',addTab);
+        }
+    })
     return(
         <div className='createContainer'>
             <div>
-            <textarea spellCheck='false' name="rant" id="rant" cols="50" rows="10" maxLength={maxLength} placeholder='Speak your mind :D ' value={rant} onChange={(e)=>setRant(e.target.value)}></textarea>
+            <textarea ref={textAreaRef} spellCheck='false' name="rant" id="rant" cols="50" rows="10" maxLength={maxLength} placeholder='Speak your mind :D ' value={rant} onChange={(e)=>setRant(e.target.value)}></textarea>
             <div id='wordCNT'>{rant.length}/{maxLength} characters used</div>
             </div>
             <div id='postBtn' onClick={sendRant}>Post</div>
